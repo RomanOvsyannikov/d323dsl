208417c8cf0042d1510308c29af919252ecc4200
Roman_Ovsyannikov <Roman_Ovsyannikov@epam.com>
2020-11-07 21:25:21 +0300

    add ActiveChoice parameters to main job

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,25 @@
+def giturl = 'https://https://github.com/RomanOvsyannikov/d323dsl'
 job ("MNTLAB-rovsyannikov-main-build-job") {
+ parameters {
+        activeChoiceParam('BRANCH_NAME') {
+            description('Choose branch')
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+              script("""("git ls-remote -h ${giturl}").execute().text.readLines().collect {
+                      it.split()[1].replaceAll(\'refs/heads/\', \'\')}.sort()""")
+            }
+        }
+        activeChoiceParam('Next_job') {
+            description('Choose job')
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''return ['MNTLAB-rovsyannikov-child1-build-job',
+                            'MNTLAB-rovsyannikov-child2-build-job',
+                            'MNTLAB-rovsyannikov-child3-build-job',
+                            'MNTLAB-rovsyannikov-child4-build-job']''')
+            }
+        }
+    }
 }
 
 for (i in (1..4)) {
